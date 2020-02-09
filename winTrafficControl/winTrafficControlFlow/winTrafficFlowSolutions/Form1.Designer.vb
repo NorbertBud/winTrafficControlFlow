fef782726eb526d322724fd965da188370f1c566
Norbert <58894482+NorbertBud@users.noreply.github.com>
2020-02-09 17:02:53 +0000

    Add files via upload

---
+++ winTrafficControl/winTrafficControlFlow/winTrafficFlowSolutions/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class mainScene
+
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mainScene))
+        Me.dtpStartDate = New System.Windows.Forms.DateTimePicker()
+        Me.dtpEndDate = New System.Windows.Forms.DateTimePicker()
+        Me.lblStartDate = New System.Windows.Forms.Label()
+        Me.lblEndDate = New System.Windows.Forms.Label()
+        Me.btnTotalTrafficflow = New System.Windows.Forms.Button()
+        Me.btnDirection = New System.Windows.Forms.Button()
+        Me.btnPercentageOfVehicleType = New System.Windows.Forms.Button()
+        Me.txtTotalTrafficflow = New System.Windows.Forms.TextBox()
+        Me.txtDirection = New System.Windows.Forms.TextBox()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.lblVehicleCounts = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'dtpStartDate
+        '
+        Me.dtpStartDate.Location = New System.Drawing.Point(44, 82)
+        Me.dtpStartDate.Name = "dtpStartDate"
+        Me.dtpStartDate.Size = New System.Drawing.Size(216, 20)
+        Me.dtpStartDate.TabIndex = 0
+        '
+        'dtpEndDate
+        '
+        Me.dtpEndDate.Location = New System.Drawing.Point(44, 34)
+        Me.dtpEndDate.Name = "dtpEndDate"
+        Me.dtpEndDate.Size = New System.Drawing.Size(216, 20)
+        Me.dtpEndDate.TabIndex = 1
+        '
+        'lblStartDate
+        '
+        Me.lblStartDate.AutoSize = True
+        Me.lblStartDate.Location = New System.Drawing.Point(95, 18)
+        Me.lblStartDate.Name = "lblStartDate"
+        Me.lblStartDate.Size = New System.Drawing.Size(136, 13)
+        Me.lblStartDate.TabIndex = 2
+        Me.lblStartDate.Text = "Please select a Start Date -"
+        '
+        'lblEndDate
+        '
+        Me.lblEndDate.AutoSize = True
+        Me.lblEndDate.Location = New System.Drawing.Point(92, 66)
+        Me.lblEndDate.Name = "lblEndDate"
+        Me.lblEndDate.Size = New System.Drawing.Size(139, 13)
+        Me.lblEndDate.TabIndex = 3
+        Me.lblEndDate.Text = "Please select an End Date -"
+        '
+        'btnTotalTrafficflow
+        '
+        Me.btnTotalTrafficflow.Location = New System.Drawing.Point(-3, 131)
+        Me.btnTotalTrafficflow.Name = "btnTotalTrafficflow"
+        Me.btnTotalTrafficflow.Size = New System.Drawing.Size(149, 23)
+        Me.btnTotalTrafficflow.TabIndex = 5
+        Me.btnTotalTrafficflow.Text = "Total Trafficflow"
+        Me.btnTotalTrafficflow.UseVisualStyleBackColor = True
+        '
+        'btnDirection
+        '
+        Me.btnDirection.Location = New System.Drawing.Point(173, 131)
+        Me.btnDirection.Name = "btnDirection"
+        Me.btnDirection.Size = New System.Drawing.Size(149, 23)
+        Me.btnDirection.TabIndex = 6
+        Me.btnDirection.Text = "Direction"
+        Me.btnDirection.UseVisualStyleBackColor = True
+        '
+        'btnPercentageOfVehicleType
+        '
+        Me.btnPercentageOfVehicleType.Location = New System.Drawing.Point(82, 199)
+        Me.btnPercentageOfVehicleType.Name = "btnPercentageOfVehicleType"
+        Me.btnPercentageOfVehicleType.Size = New System.Drawing.Size(149, 25)
+        Me.btnPercentageOfVehicleType.TabIndex = 8
+        Me.btnPercentageOfVehicleType.Text = "Number Of Vehicle Types"
+        Me.btnPercentageOfVehicleType.UseVisualStyleBackColor = True
+        '
+        'txtTotalTrafficflow
+        '
+        Me.txtTotalTrafficflow.Location = New System.Drawing.Point(12, 160)
+        Me.txtTotalTrafficflow.Name = "txtTotalTrafficflow"
+        Me.txtTotalTrafficflow.ReadOnly = True
+        Me.txtTotalTrafficflow.Size = New System.Drawing.Size(119, 20)
+        Me.txtTotalTrafficflow.TabIndex = 9
+        '
+        'txtDirection
+        '
+        Me.txtDirection.Location = New System.Drawing.Point(191, 160)
+        Me.txtDirection.Name = "txtDirection"
+        Me.txtDirection.ReadOnly = True
+        Me.txtDirection.Size = New System.Drawing.Size(119, 20)
+        Me.txtDirection.TabIndex = 10
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(31, 301)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(100, 23)
+        Me.btnCancel.TabIndex = 15
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'lblVehicleCounts
+        '
+        Me.lblVehicleCounts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblVehicleCounts.Location = New System.Drawing.Point(106, 227)
+        Me.lblVehicleCounts.Name = "lblVehicleCounts"
+        Me.lblVehicleCounts.Size = New System.Drawing.Size(100, 60)
+        Me.lblVehicleCounts.TabIndex = 16
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(191, 301)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(100, 23)
+        Me.btnSave.TabIndex = 17
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'mainScene
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(334, 347)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lblVehicleCounts)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.txtDirection)
+        Me.Controls.Add(Me.txtTotalTrafficflow)
+        Me.Controls.Add(Me.btnPercentageOfVehicleType)
+        Me.Controls.Add(Me.btnDirection)
+        Me.Controls.Add(Me.btnTotalTrafficflow)
+        Me.Controls.Add(Me.lblEndDate)
+        Me.Controls.Add(Me.lblStartDate)
+        Me.Controls.Add(Me.dtpEndDate)
+        Me.Controls.Add(Me.dtpStartDate)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "mainScene"
+        Me.Text = "Total Traffic Flow"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dtpStartDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dtpEndDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents lblStartDate As System.Windows.Forms.Label
+    Friend WithEvents lblEndDate As System.Windows.Forms.Label
+    Friend WithEvents btnTotalTrafficflow As System.Windows.Forms.Button
+    Friend WithEvents btnDirection As System.Windows.Forms.Button
+    Friend WithEvents btnPercentageOfVehicleType As System.Windows.Forms.Button
+    Friend WithEvents txtTotalTrafficflow As System.Windows.Forms.TextBox
+    Friend WithEvents txtDirection As System.Windows.Forms.TextBox
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents lblVehicleCounts As System.Windows.Forms.Label
+    Friend WithEvents btnSave As Button
+End Class
+
---
+++ winTrafficControl/winTrafficControlFlow/winTrafficFlowSolutions/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Public Class mainScene
+
+    Dim SimulatedTrafficData As String = "C:\Users\PC\Desktop\Software Development\winTrafficControl\winTrafficControlFlow\winTrafficFlowSolutions\TrafficData.txt" 'this adds the variable into the prgramme(This is where the program gets its data from, please change it based on your directory of the "TrafficData.txt" file).
+
+    Private Sub btnTotalTrafficflow_Click(sender As Object, e As EventArgs) Handles btnTotalTrafficflow.Click
+
+
+        File.Exists(SimulatedTrafficData) 'this line fo code makes sure that the text file exists
+        Dim streamR As StreamReader 'this allows the computer to read the file
+        streamR = File.OpenText(SimulatedTrafficData) 'this declares sr will read the data file
+        Dim record As String 'this declares 'record' as a string
+        Dim totalVehicleFlow As Integer 'this declares 'totalVehicleFlow' as an integer
+
+        Do While streamR.Peek() <> -1
+
+            record = streamR.ReadLine 'this will read a line from the data file
+            totalVehicleFlow += 1 'this will add 1 to the total traffic flow after each line is read
+        Loop
+
+        txtTotalTrafficflow.Text = CStr(totalVehicleFlow) 'this line of code is made so the user can see the output in text.
+
+    End Sub
+
+    Private Sub btnPercentageOfVehicleType_Click(sender As Object, e As EventArgs) Handles btnPercentageOfVehicleType.Click
+
+        Dim currRecord As String 'to store a current record in the variable "currRecord"
+        Dim currWeight As Integer 'this is an integer that will hold the weight of each vehicle
+        Dim recordArray() As String 'this is to record each array
+        Dim motorBikeCount As Integer 'this counts how many motorbikes are there based on weight
+        Dim carCount As Integer 'this counts how many cars are there based on weight
+        Dim vanCount As Integer 'this counts how many vans are there based on weight
+        Dim lorryCount As Integer 'this counts how many lorries are there based on weight
+
+        'This will assign "StreamReader" to "sr" so we can use it more efficiently as you dont have to type the whole syntax of the function.
+        Dim sr As StreamReader
+
+        'this step checks if the file actually exists and if it does then all the calculations can be made
+        Try
+            If File.Exists(SimulatedTrafficData) Then
+                sr = File.OpenText(SimulatedTrafficData) 'this line of code reads whats inside the file
+
+                Do While sr.Peek() <> -1 'if the file to read is not empty
+                    currRecord = sr.ReadLine
+                    recordArray = currRecord.Split(","c) 'this line of code assigns every line to arrays, index 0, index 1 etc.
+
+                    currWeight = recordArray(3).ToString 'store the current weight in the ages array at current index
+
+                    If currWeight > 1 And currWeight < 699 Then 'this declares the weight values for a bike
+                        motorBikeCount += 1 'this is a counter that collects the data
+                    ElseIf currWeight > 700 And currWeight < 4600 Then 'this declares the weight values for a car
+                        carCount += 1 'this is a counter that collects the data
+                    ElseIf currWeight > 4601 And currWeight < 7899 Then 'this declares the weight values for a van
+                        vanCount += 1 'this is a counter that collects the data
+                    ElseIf currWeight > 7900 And currWeight < 20000 Then 'this declares the weight values for a lorry
+                        lorryCount += 1 'this is a counter that collects the data
+
+                    End If
+
+                Loop
+                sr.Close()
+                'display the calculations after the loop
+                lblVehicleCounts.Text = "Motor Bikes: " & motorBikeCount 'this will display the result of number of bikes and give a name for the result.
+                lblVehicleCounts.Text &= Chr(13) 'this will spereate the results. (put them on seperate line)
+                lblVehicleCounts.Text &= "Cars: " & carCount 'this will display the result of number of cars and give a name for the result.
+                lblVehicleCounts.Text &= Chr(13) 'this will spereate the results. (put them on seperate line)
+                lblVehicleCounts.Text &= "Vans: " & vanCount 'this will display the result of number of vans and give a name for the result.
+                lblVehicleCounts.Text &= Chr(13) 'this will spereate the results. (put them on seperate line)
+                lblVehicleCounts.Text &= "Lorry: " & lorryCount 'this will display the result of number of lorrys and give a name for the result.
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click 'when user clicks the button this function is executed
+        Try 'this is just a shortuct as we assign the file location into a variable "fileLocation"
+            Dim fileLocation As String = "C:\Users\PC\Desktop\Software Development\winTrafficControl\winTrafficControlFlow\winTrafficFlowSolutions\Report.txt"
+
+            If File.Exists("C:\Users\PC\Desktop\Software Development\winTrafficControl\winTrafficControlFlow\winTrafficFlowSolutions\Report.txt") Then 'this checks if the file is in that location
+                Dim sWriter As StreamWriter 'another shortcut so we dont have to repeat "StreamWriter" all the time.
+                sWriter = File.AppendText(fileLocation) 'this line of code creates a StreamWriter tat appends UTF-8 encoded text to an existing file.
+                sWriter.WriteLine("Report1: Traffic Vehicle Counts:" & Chr(13) & System.DateTime.Now) 'this writes a line to the text file that says "Report1: Traffic Vehicle Counts: ***" and outputs date
+                sWriter.WriteLine(lblVehicleCounts.Text & ", Total vehicle flow on that day: " & txtTotalTrafficflow.Text) 'This line of code will output all the vehicles as well as traffic flow for a specific text file into a report.
+                sWriter.Close() 'closes the StreamReader"
+                MsgBox("Result written to file sucessfully!") 'this will be displayed if everything was performed correctly.
+            Else
+                MsgBox("Something went wrong!") 'if the program was't able to find the file it will display an error.
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        MsgBox("The program will now close") ' this function closes users program on their demand and outputs a text message.
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnPercentageOfVehicleType_ContextMenuChanged(sender As Object, e As EventArgs) Handles btnPercentageOfVehicleType.ContextMenuChanged
+
+    End Sub
+End Class
